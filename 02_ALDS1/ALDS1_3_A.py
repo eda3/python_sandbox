e1a2da02161576d56df9f217c27851c94b40a1f1
eda3 <gerorin1010@gmail.com>
2020-07-18 16:55:51 +0900

    commit

---
+++ 02_ALDS1/ALDS1_3_A.py
@@ -0,0 +1,12 @@
+from collections import deque
+d = deque()
+li = list(input().split())
+for i in li:
+    if i in "+-*":
+        b = d.pop()
+        a = d.pop()
+        d.append(str(eval(a + i + b)))
+    else:
+        d.append(i)
+
+print(str(d[0]))
