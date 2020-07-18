4acf9646f493b404f89d58bcd55a06fb68de2e8b
eda3 <gerorin1010@gmail.com>
2020-07-18 10:14:38 +0900

    commit

---
+++ 02_ALDS1/ALDS1_1_C.py
@@ -0,0 +1,18 @@
+def is_prime(q, k=50):
+    q = abs(q)
+    if q == 0:
+        return False
+    elif q == 2:
+        return True
+    return pow(2, q-1, q) == 1
+
+
+n = int(input())
+result = 0
+for i in range(n):
+    x = int(input())
+    if is_prime(x):
+        result += 1
+
+print(result)
+
