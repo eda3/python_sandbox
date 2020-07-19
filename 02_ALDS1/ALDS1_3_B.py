4f4033f7d63f62fd9a67f4c65078dbaa69b09c49
eda3 <gerorin1010@gmail.com>
2020-07-19 13:17:34 +0900

    commit

---
+++ 02_ALDS1/ALDS1_3_B.py
@@ -0,0 +1,19 @@
+from collections import deque
+n, q_time = map(int, input().split())
+d = deque()
+for i in range(n):
+    x, y = input().split()
+    d.append((x, int(y)))
+
+total = 0
+while d:
+    name, time = d.popleft()
+    if time <= q_time:
+        total += time
+        print(name, total)
+    else:
+        total += q_time
+        time -= q_time
+        d.append((name, time))
+
+
