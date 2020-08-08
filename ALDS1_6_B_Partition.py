44c03a30d5027be5afe397952938276e6a435d3a
eda3 <gerorin1010@gmail.com>
2020-08-08 13:40:07 +0900

    commit

---
+++ ALDS1_6_B_Partition.py
@@ -0,0 +1,24 @@
+input()
+A = [int(x) for x in list(input().split())]
+# 数列の最後の値を基準値にする
+x = A[-1]
+i = -1
+
+r = len(A)
+# 数列の一番最後の一個手前まで繰り返す
+for j in range(r):
+    print(f"{A=}")
+    print(f"{i=}")
+    print(f"{j=}")
+    # print(f"{A[j]=}")
+    # 各値がxより大きいか小さいかで振り分ける
+    if A[j] <= x:
+        i += 1
+        # A[i]とA[j]を交換
+        A[i], A[j] = A[j], A[i]
+    print(f"{A=}")
+    print()
+
+# 基準値の前後に[ ]を追加
+A[i] = "[" + str(A[i]) + "]"
+print(*A)
